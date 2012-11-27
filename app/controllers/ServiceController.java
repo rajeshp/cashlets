9d2bf11530824509b2342c2d2e29e9dc6ca2bdf1
Rajesh Rao <thinkmein2005@gmail.com>
2012-11-27 22:51:17 +0530

    changes for adding pictures to service

--- app/controllers/ServiceController.java
+++ app/controllers/ServiceController.java
@@ -1,7 +1,15 @@
 package controllers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Date;
 
+import models.Photo;
+import org.hibernate.type.descriptor.BinaryStream;
+import play.db.jpa.Blob;
+import play.db.jpa.JPA;
+import play.libs.MimeTypes;
 import play.modules.morphia.Model;
 import play.mvc.Controller;
 import play.mvc.With;
@@ -12,10 +20,13 @@ import securesocial.provider.SocialUser;
 import controllers.securesocial.SecureSocial;
 
 @With(SecureSocial.class)
+
+
+
 public class ServiceController extends Controller {
 
 	
-	public static void AddService( String title, String description, String refURL, float price, String zipcode,String phone, String email)
+	public static void AddService( String title, String description, String refURL, float price, String zipcode,String phone, String email, File photo)
     {
 
         System.out.println("************ ADD Service Controller ***************");
@@ -38,6 +49,34 @@ public class ServiceController extends Controller {
         Date today = new Date();
         newService.createdOnDate= today; 
         newService.lastModifieDate= today;
+
+
+        Photo servicePic = new Photo();
+            try
+            {
+
+                String fileType = MimeTypes.getContentType(photo.getName());
+                Blob fileBinary = new Blob();
+
+                fileBinary.set(new FileInputStream(photo),fileType);
+
+
+                servicePic.photo_binary=fileBinary;
+                servicePic.addedOn = new Date();
+
+
+                servicePic.save();
+
+            }
+            catch(IOException io)
+            {
+                System.out.println("Error Uploading image");
+                servicePic.delete();
+
+
+            }
+
+
         newService.save();
         }
 
--- app/models/Photo.java
+++ app/models/Photo.java
@@ -1,27 +1,23 @@
 package models;
 
+import com.google.code.morphia.annotations.*;
+import com.google.code.morphia.annotations.Id;
 import play.db.jpa.Blob;
 import java.util.Date;
 
-import com.google.code.morphia.annotations.Entity;
-
-
 
 /**
- * Created with IntelliJ IDEA.
- * User: TE162141
- * Date: 10/25/12
- * Time: 7:31 PM
- * To change this template use File | Settings | File Templates.
- */
+* Created with IntelliJ IDEA.
+* User: TE162141
+* Date: 10/25/12
+* Time: 7:31 PM
+* To change this template use File | Settings | File Templates.
+*/
 @Entity
 public class Photo extends Model {
-	   
-    String photo_id;
-    Blob photo_binary;
-    Date addedOn;
-    Date lastModifiedDate;
 
 
+    public static Blob photo_binary;
+    public static Date addedOn;
 
 }
